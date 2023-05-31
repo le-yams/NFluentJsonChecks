aee4b4f62142fc887507a15ac3d93bf06030a2d7
Yann D'Isanto <ydisanto@gmail.com>
2023-05-31 11:20:51 +0200

    feat: json checks

---
+++ NFluentJsonChecks.Tests/TestJson.cs
@@ -0,0 +1,14 @@
+﻿using System.Net.Http.Json;
+using System.Text.Json;
+
+namespace NFluentJsonChecks.Tests;
+
+public static class TestJson
+{
+    
+    public static async Task<JsonElement> Element(object obj)
+    {
+        var json = await JsonDocument.ParseAsync(await JsonContent.Create(obj).ReadAsStreamAsync());
+        return json.RootElement;
+    }
+}
